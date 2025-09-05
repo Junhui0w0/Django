641d81ecc438919bbd3ff307add5d58175212dc0
Junhui0w0 <wydyxh@gmail.com>
2025-09-05 19:26:52 +0900

    [feat] 페이징 추가

--- projects/mysite/pybo/views.py
+++ projects/mysite/pybo/views.py
@@ -4,11 +4,22 @@ from django.utils import timezone
 from .models import Question, Answer
 from .forms import QuestionForm, AnswerForm
 
+from django.core.paginator import Paginator #- 한 페이지에 여러 개의 결과를 보여주는 것 방지
 
-def index(request): #게시글 리스트
-    question_lst  = Question.objects.order_by('-create_date') # -create_date = 역방향 / create_date=정방향
-    context = {'question_list':question_lst}
+def index(request): #게시글 리스트 -> 한 페이지당 최대 10개의 게시글 출력하도록 할 것
+    # question_lst  = Question.objects.order_by('-create_date') # -create_date = 역방향 / create_date=정방향
+    # context = {'question_list':question_lst}
+    # return render(request, 'pybo/question_list.html', context)
+
+    page = request.GET.get('page', '1') #page 값을 가져온다. 이때, 기본값은 1
+    question_list = Question.objects.order_by('-create_date') #전체 게시글을 create_date 역순을 기준으로 불러옴
+
+    paginator = Paginator(question_list, 10) #불러온 게시글을 10개씩 나눔
+    page_obj = paginator.get_page(page) #page번호에 해당하는 게시글들 불러옴
+    context = {'question_list': page_obj} #해당 page에 있는 게시글들 정보를 context에 저장
+
     return render(request, 'pybo/question_list.html', context)
+
 
 def detail(request, question_id): #게시글 상세
     # question = Question.objects.get(id=question_id)
