1105d3943aeff6444a3b41a6ac4e9c4ea14671bd
Junhui0w0 <wydyxh@gmail.com>
2025-09-07 22:14:04 +0900

    사전접수, 찾기 Form 추가

--- projects/mysite/pybo/forms.py
+++ projects/mysite/pybo/forms.py
@@ -1,5 +1,65 @@
 from django import forms
-from pybo.models import Question, Answer
+from pybo.models import Question, Answer, Pre_Question, Find_Question
+
+PACKAGE_CHOICES = [
+    ('naked', '나체'),
+    ('box', '박스'),
+    ('vinyl', '비닐'),
+    ('madae', '마대'),
+    ('custom', '직접 입력'),  # 추가!
+]
+
+NUM_CHOICES = [
+    ('1', '1'),
+    ('2', '2'),
+    ('3', '3'),
+    ('4', '4'),
+    ('5', '5'),
+    ('custom', '직접 입력'),
+]
+
+class Pre_QuestionForm(forms.ModelForm):
+    package_type = forms.ChoiceField(choices=PACKAGE_CHOICES, label='포장 형식')
+    num = forms.ChoiceField(choices=NUM_CHOICES, label='수량')  # 1부터 10까지의 수량 선택지
+
+    class Meta: #ModelForm에서는 필수
+        model = Pre_Question #사용 할 모델
+        fields = ['subject', 'send_name', 'send_phone', 'send_addr', 'rec_name', 'rec_phone', 'rec_addr', 'product_type', 'package_type', 'num'] #QuestionForm에서 사용할 Queston 모델의 속성들
+        # widgets = {
+        #     'subject': forms.TextInput(attrs={'class': 'form-control'}),
+        #     'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 10}),
+        # }
+        labels = {
+            'subject': '제목',
+            'send_name': '보내는 분 성함',
+            'send_phone': '보내는 분 연락처',
+            'send_addr': '보내는 분 주소',
+            'rec_name': '받는 분 성함',
+            'rec_phone': '받는 분 연락처',
+            'rec_addr': '받는 분 주소',
+            'product_type': '상품 종류',
+            'package_type': '포장 형식',
+            'num': '수량',
+        }  
+
+class Find_QuestionForm(forms.ModelForm):
+    package_type = forms.ChoiceField(choices=PACKAGE_CHOICES, label='포장 형식')
+    num = forms.ChoiceField(choices=NUM_CHOICES, label='수량') 
+
+    class Meta: #ModelForm에서는 필수
+        model = Find_Question #사용 할 모델
+        fields = ['subject', 'send_name', 'send_phone', 'send_addr', 'product_type', 'package_type', 'num'] #QuestionForm에서 사용할 Queston 모델의 속성들
+
+        labels = {
+            'subject': '제목',
+            'send_name': '보내는 분 성함',
+            'send_phone': '보내는 분 연락처',
+            'send_addr': '보내는 분 주소',
+            'product_type': '상품 종류',
+            'package_type': '포장 형식',
+            'num': '수량',
+        } 
+
 
 class QuestionForm(forms.ModelForm):
     class Meta: #ModelForm에서는 필수
