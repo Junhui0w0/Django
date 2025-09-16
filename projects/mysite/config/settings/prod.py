8d4ca9f8371d65f1efa4462f6c33df031bfe49c3
Junhui0w0 <wydyxh@gmail.com>
2025-09-16 12:17:41 +0900

    dns 연결

--- projects/mysite/config/settings/prod.py
+++ projects/mysite/config/settings/prod.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['3.35.96.107']
+ALLOWED_HOSTS = ['3.35.96.107', 'pajudaesin.quest']
 
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
